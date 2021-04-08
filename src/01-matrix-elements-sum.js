f27a75377b94ac7893efdd36edada99d2e5b5ae7
vadimshut <vadimshut1992@gmail.com>
2021-04-08 12:59:01 +0300

    fix: fixed 01 function

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -19,14 +19,14 @@ function getMatrixElementsSum(matrix) {
   const arr = [];
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] == 0) {
+      if (matrix[i][j] === 0) {
         arr.push(j);
-      } else if (arr.indexOf(j) == -1) {
+      } else if (arr.indexOf(j) === -1) {
         sum += matrix[i][j];
       }
     }
   }
-  return total;
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
