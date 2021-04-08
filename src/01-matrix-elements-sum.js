d3b5f6613afc5b5cea12880e4d9e75384ce9ee16
vadimshut <vadimshut1992@gmail.com>
2021-04-08 12:55:33 +0300

    feat: implement 01 function

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,19 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == 0) {
+        arr.push(j);
+      } else if (arr.indexOf(j) == -1) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return total;
 }
 
 module.exports = getMatrixElementsSum;
