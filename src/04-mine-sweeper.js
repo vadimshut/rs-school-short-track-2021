5d865e26973f1975988a5603c0a573070467a53e
vadimshut <vadimshut1992@gmail.com>
2021-04-08 17:18:53 +0300

    feat: implement 04 function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,30 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const lenMatrix = matrix.length;
+  const lenInsideMatrix = matrix[0].length;
+  const result = new Array(lenMatrix);
+
+  for (let i = 0; i < lenMatrix; ++i) {
+    result[i] = new Array(lenInsideMatrix).fill(0);
+  }
+
+  for (let i = 0; i < lenMatrix; ++i) {
+    for (let j = 0; j < lenInsideMatrix; ++j) {
+      if (matrix[i][j] === true) {
+        if (i > 0 && j > 0) result[i - 1][j - 1] += 1;
+        if (i > 0) result[i - 1][j] += 1;
+        if (j > 0) result[i][j - 1] += 1;
+        if (i > 0 && j < lenInsideMatrix - 1) result[i - 1][j + 1] += 1;
+        if (i < lenMatrix - 1 && j > 0) result[i + 1][j - 1] += 1;
+        if (i < lenMatrix - 1) result[i + 1][j] += 1;
+        if (j < lenInsideMatrix - 1) result[i][j + 1] += 1;
+        if (i < lenMatrix - 1 && j < lenInsideMatrix - 1) result[i + 1][j + 1] += 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
