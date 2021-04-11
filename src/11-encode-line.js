1741fc31a4c1817cf260bb5074b190404832df7e
vadimshut <vadimshut1992@gmail.com>
2021-04-11 18:42:45 +0300

    feat: implement function 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const inputStr = str;
+  let resultStr = '';
+  let count = 1;
+  const strArr = inputStr.split('');
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === strArr[i + 1]) {
+      count += 1;
+    } else {
+      resultStr += count > 1 ? (count + strArr[i]) : strArr[i];
+      count = 1;
+    }
+  }
+  return resultStr;
 }
-
 module.exports = encodeLine;
